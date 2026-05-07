ee839981cc2f70b6db9f88c3be771d718c3c6860
vivtorochok <vika17052001@gmail.com>
2026-05-07 12:28:13 +0300

    style-update-bestsellers

--- src/js/api-requests/getBestsellers.js
+++ src/js/api-requests/getBestsellers.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import { getLimitByScreen } from '../bestsellers/handlers';
 
-export const getBestsellers = async page => {
+export const getBestsellers = async (page, limit) => {
   const url = `https://deserts-store.b.goit.study/api`;
 
   const { data } = await axios.get(`${url}/desserts?type=popular`, {
     params: {
       page,
-      limit: 3,
+      limit,
     },
   });
 
