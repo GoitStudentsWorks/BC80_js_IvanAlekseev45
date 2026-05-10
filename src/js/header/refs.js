472d6b0c82c049be1b9c4503a238a90c2ebaff83
Ordinateur 1 <abcvika3@gmail.com>
2026-05-10 16:43:56 +0200

    add feat-header to fix logo

---
+++ src/js/header/refs.js
@@ -0,0 +1,13 @@
+export const refsHeader = {
+  burgerBtn: document.querySelector('.open-burger-btn'),
+
+  burgerMenu: document.querySelector('.burger-menu'),
+
+  closeBurgerBtn: document.querySelector('.close-burger-icon'),
+
+  burgerLogoLinkMain: document.querySelector('.logo-link-burger'),
+
+  burgerLinkSection: document.querySelectorAll('.burger-nav-link'),
+
+  burgerOrderBtn: document.querySelector('.burger-order-btn'),
+};
